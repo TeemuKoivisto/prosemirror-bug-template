2b4cbe4d70d7f72a7f9647e272a13938aec850b1
Teemu <tkoivisto456@gmail.com>
2022-05-05 15:40:27 +0300

    styles: run prettier

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-import { EditorState } from "prosemirror-state";
-import { EditorView } from "prosemirror-view";
-import { exampleSetup } from "prosemirror-example-setup";
+import { EditorState } from 'prosemirror-state'
+import { EditorView } from 'prosemirror-view'
+import { exampleSetup } from 'prosemirror-example-setup'
 
 import { schema } from './schema.js'
 
 let docJson
-(async () => {
+;(async () => {
   docJson = await fetch('./doc.json').then((res) => res.json())
 })()
 
 const state = EditorState.create({
   schema,
-  plugins: exampleSetup({ schema })
-});
-const view = new EditorView(document.querySelector("#editor"), {
-  state
-});
-applyDevTools(view);
+  plugins: exampleSetup({ schema }),
+})
+const view = new EditorView(document.querySelector('#editor'), {
+  state,
+})
+applyDevTools(view)
 
 const reset = (view) => {
-  const { dispatch, state } = view;
-  const { tr } = state;
-  const doc = state.schema.nodeFromJSON(docJson);
-  tr.replaceWith(0, state.doc.nodeSize - 2, doc);
-  dispatch(tr);
-};
+  const { dispatch, state } = view
+  const { tr } = state
+  const doc = state.schema.nodeFromJSON(docJson)
+  tr.replaceWith(0, state.doc.nodeSize - 2, doc)
+  dispatch(tr)
+}
 
-document.querySelector("#reset-btn")?.addEventListener("click", () => {
-  reset(view);
-});
+document.querySelector('#reset-btn')?.addEventListener('click', () => {
+  reset(view)
+})
